c34d8c2670733b6b95d0b61d595ed798b491eeba
Daniel Naranjo <danielnaranjo@users.noreply.github.com>
2016-09-17 12:24:35 -0300

    Remove statics routes added by mistake

--- api/routes.php
+++ api/routes.php
@@ -1,23 +1,2 @@
 <?php
-/* dynamic routes */
-include('routes/_commons.php');
-include('routes/audio.php');
-include('routes/audit.php');
-include('routes/balance.php');
-include('routes/branch.php');
-include('routes/city.php');
-include('routes/company.php');
-include('routes/country.php');
-include('routes/image.php');
-include('routes/province.php');
-include('routes/quiz.php');
-include('routes/quiz_answer.php');
-include('routes/quiz_option.php');
-include('routes/quiz_option_selected.php');
-include('routes/task.php');
-include('routes/user.php');
-include('routes/user_company.php');
-include('routes/user_image.php');
-include('routes/user_info.php');
-include('routes/withdraw.php');
-/* dynamic routes */
+/* dynamic routes goes here */
